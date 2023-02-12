c6bfd580313ad494ffc8c2000b80a7d4a77e1327
Oskar Hane <oh@oskarhane.com>
2023-02-12 16:46:49 +0100

    opt-in to enabling SSL

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-	plugins: [sveltekit(), basicSsl()]
+	plugins: [sveltekit(), process.env.ENABLE_SSL ? basicSsl() : undefined]
 });
